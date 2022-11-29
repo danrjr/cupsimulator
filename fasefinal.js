5d0c1ccf409b24a2c5c013a8036d26721b489cc8
danrjr <danielreis35@gmail.com>
2022-11-29 08:45:39 -0300

    first commit

---
+++ fasefinal.js
@@ -0,0 +1,256 @@
+const _ = require('lodash');
+
+const {a1, a2} = require('./gA')
+const {b1, b2} = require('./gB')
+const {c1, c2} = require('./gC')
+const {d1, d2} = require('./gD')
+const {e1, e2} = require('./gE')
+const {f1, f2} = require('./gF')
+const {g1, g2} = require('./gG')
+const {h1, h2} = require('./gH')
+
+let a1ff = a1.nome
+let a2ff = a2.nome 
+let b1ff = b1.nome
+let b2ff = b2.nome 
+let c1ff = c1.nome
+let c2ff = c2.nome 
+let d1ff = d1.nome
+let d2ff = d2.nome 
+let e1ff = e1.nome
+let e2ff = e2.nome 
+let f1ff = f1.nome
+let f2ff = f2.nome 
+let g1ff = g1.nome
+let g2ff = g2.nome 
+let h1ff = h1.nome
+let h2ff = h2.nome 
+
+// console.log(a1ff, a2ff)
+// console.log(b1ff, b2ff)
+// console.log(c1ff, c2ff)
+// console.log(d1ff, d2ff)
+// console.log(e1ff, e2ff)
+// console.log(f1ff, f2ff)
+// console.log(g1ff, g2ff)
+// console.log(h1ff, h2ff)
+
+arr0 = [] 
+arr1 = [] 
+arr2 = [] 
+arr3 = [] 
+arr4 = [] 
+arr5 = [] 
+arr6 = [] 
+arr7 = [] 
+arr8 = [] 
+arr9 = [] 
+arr10 = [] 
+arr11 = [] 
+arr12 = [] 
+arr13 = [] 
+arr14 = [] 
+arr15 = [] 
+arr16 = []
+p0 = [] 
+p1 = [] 
+p2 = [] 
+p3 = [] 
+p4 = [] 
+p5 = [] 
+p6 = [] 
+p7 = [] 
+p8 = [] 
+p9 = [] 
+p10 = [] 
+p11 = [] 
+p12 = [] 
+p13 = [] 
+p14 = [] 
+p15 = [] 
+
+function geraGolsForte(array){ 
+    while (array.length < 1){ 
+        var gols=[0,1,2,3,4,5,6,7,8]
+        var golsponderados = [0.7, 0.9, 3.5, 2.25, 2.25, 0.1, 0.1, 0.1, 0.1]
+        var totalponderado = eval(golsponderados.join("+"))
+        var ponderacao = new Array()
+        var golatual = 0;
+    
+        while (golatual<gols.length){ //step through each fruit[] element
+            for (i=0; i<golsponderados[golatual]; i++)
+                ponderacao[ponderacao.length]=gols[golatual]
+            golatual++
+        }
+        var randomnumber=Math.floor(Math.random()*totalponderado)
+
+         array.push(ponderacao[randomnumber]) 
+    } 
+} 
+
+function geraGolsMed(array){ 
+    while (array.length < 1){ 
+        var gols=[0,1,2,3,4]
+        var golsponderados = [4.0,4.0,1.0,0.5,0.5]
+        var totalponderado = eval(golsponderados.join("+"))
+        var ponderacao = new Array();
+        var golatual = 0;
+    
+        while (golatual<gols.length){ //step through each fruit[] element
+            for (i=0; i<golsponderados[golatual]; i++)
+                ponderacao[ponderacao.length]=gols[golatual]
+            golatual++
+        }
+        var randomnumber=Math.floor(Math.random()*totalponderado)
+
+         array.push(ponderacao[randomnumber]) 
+    }
+}
+
+function penalti(array){ 
+    while (array.length < 5){ 
+        array.push(Math.floor(Math.random() * (2 - 0) ) + 0) 
+    }
+}
+
+geraGolsForte(arr0) 
+geraGolsMed(arr1) 
+geraGolsForte(arr2) 
+geraGolsMed(arr3) 
+geraGolsForte(arr4) 
+geraGolsMed(arr5) 
+geraGolsForte(arr6) 
+geraGolsMed(arr7) 
+geraGolsForte(arr8) 
+geraGolsMed(arr9) 
+geraGolsForte(arr10) 
+geraGolsMed(arr11) 
+geraGolsForte(arr12) 
+geraGolsMed(arr13) 
+geraGolsForte(arr14) 
+geraGolsMed(arr15) 
+geraGolsForte(arr16) 
+
+penalti(p0)
+penalti(p1)
+penalti(p2)
+penalti(p3)
+penalti(p4)
+penalti(p5)
+penalti(p6)
+penalti(p7)
+penalti(p8)
+penalti(p9)
+penalti(p10)
+penalti(p11)
+penalti(p12)
+penalti(p13)
+penalti(p14)
+penalti(p15)
+
+const sum_penalti = (arr) => arr.reduce((acc,next) => acc +=next, 0)
+
+let oitavas = [
+    {nome: a1ff, gol: arr0, penalti: p0, total_penalti: sum_penalti(p0)},
+    {nome: b2ff, gol: arr1, penalti: p1, total_penalti: sum_penalti(p1)},
+    {nome: c1ff, gol: arr2, penalti: p2, total_penalti: sum_penalti(p2)},
+    {nome: d2ff, gol: arr3, penalti: p3, total_penalti: sum_penalti(p3)},
+    {nome: e1ff, gol: arr4, penalti: p4, total_penalti: sum_penalti(p4)},
+    {nome: f2ff, gol: arr5, penalti: p5, total_penalti: sum_penalti(p5)},
+    {nome: g1ff, gol: arr6, penalti: p6, total_penalti: sum_penalti(p6)},
+    {nome: h2ff, gol: arr7, penalti: p7, total_penalti: sum_penalti(p7)},
+    {nome: b1ff, gol: arr8, penalti: p8, total_penalti: sum_penalti(p8)},
+    {nome: a2ff, gol: arr9, penalti: p9, total_penalti: sum_penalti(p9)},
+    {nome: d1ff, gol: arr10, penalti: p10, total_penalti: sum_penalti(p10)},
+    {nome: c2ff, gol: arr11, penalti: p11, total_penalti: sum_penalti(p11)},
+    {nome: f1ff, gol: arr12, penalti: p12, total_penalti: sum_penalti(p12)},
+    {nome: e2ff, gol: arr13, penalti: p13, total_penalti: sum_penalti(p13)},
+    {nome: h1ff, gol: arr14, penalti: p14, total_penalti: sum_penalti(p14)},
+    {nome: g2ff, gol: arr15, penalti: p15, total_penalti: sum_penalti(p15)},
+]
+
+let quartas = [
+    {nome: "", gol: arr0, penalti: p0, total_penalti: sum_penalti(p0)},
+    {nome: "", gol: arr2, penalti: p1, total_penalti: sum_penalti(p1)},
+    {nome: "", gol: arr4, penalti: p2, total_penalti: sum_penalti(p2)},
+    {nome: "", gol: arr8, penalti: p3, total_penalti: sum_penalti(p3)},
+    {nome: "", gol: arr10, penalti: p4, total_penalti: sum_penalti(p4)},
+    {nome: "", gol: arr12, penalti: p5, total_penalti: sum_penalti(p5)},
+    {nome: "", gol: arr14, penalti: p6, total_penalti: sum_penalti(p6)},
+    {nome: "", gol: arr0, penalti: p7, total_penalti: sum_penalti(p7)},
+]
+
+let semi = [
+    {nome: null, gol: arr0, penalti: 0, total_penalti: 0},
+    {nome: null, gol: arr2, penalti: 0, total_penalti: 0},
+    {nome: null, gol: arr4, penalti: 0, total_penalti: 0},
+    {nome: null, gol: arr8, penalti: 0, total_penalti: 0},
+]
+
+let terceiro_lugar = [
+    {nome: null, gol: arr0, penalti: 0, total_penalti: 0},
+    {nome: null, gol: arr2, penalti: 0, total_penalti: 0},
+]
+
+let final = [
+    {nome: null, gol: arr0, penalti: 0, total_penalti: 0},
+    {nome: null, gol: arr2, penalti: 0, total_penalti: 0},
+]
+
+function gerarConfronto(posi, posp, posq, pos0, pos1){
+    if(posi[pos0]['gol'] > posi[pos1]['gol']){
+        posp[posq]['nome'] = posi[pos0]['nome']
+    }
+    else if(posi[pos0]['gol'] < posi[pos1]['gol']){
+        posp[posq]['nome'] = posi[pos1]['nome']
+    }
+    else{
+        if(posi[pos0]['total_penalti'] > posi[pos1]['total_penalti']){
+           posp[posq]['nome'] = posi[pos0]['nome']
+        }
+        else if(posi[pos0]['total_penalti'] < posi[pos1]['total_penalti']){
+            posp[posq]['nome'] = posi[pos1]['nome']
+        }
+        else{
+            let tie = Math.floor(Math.random() * (2 - 0) ) + 0
+            if(tie == 1){
+                posp[posq]['nome'] = posi[pos0]['nome']
+            }
+            else{
+                posp[posq]['nome'] = posi[pos1]['nome']
+            }
+        }
+    }
+}
+
+
+
+gerarConfronto(oitavas, quartas, 0,0,1)
+gerarConfronto(oitavas, quartas, 1,2,3)
+gerarConfronto(oitavas, quartas, 2,4,5)
+gerarConfronto(oitavas, quartas, 3,6,7)
+gerarConfronto(oitavas, quartas, 4,8,9)
+gerarConfronto(oitavas, quartas, 5,10,11)
+gerarConfronto(oitavas, quartas, 6,12,13)
+gerarConfronto(oitavas, quartas, 7,14,15)
+//console.log(oitavas[0]['gol'])
+gerarConfronto(quartas, semi, 0,0,1)
+gerarConfronto(quartas, semi, 1,2,3)
+gerarConfronto(quartas, semi, 2,4,5)
+gerarConfronto(quartas, semi, 3,6,7)
+
+gerarConfronto(semi, final, 0,0,1);
+gerarConfronto(semi, final, 1,2,3);
+
+
+
+
+oitavas.map(q => console.log(q.nome));
+console.log('////////')
+quartas.map(q => console.log(q.nome));
+console.log('////////')
+semi.map(q => console.log(q.nome));
+console.log('////////')
+final.map(q => console.log(q.nome));
+console.log('////////')
+console.log(quartas)
---
+++ gH.js
@@ -0,0 +1,372 @@
+const _ = require('lodash')
+arr0 = [] 
+arr1 = [] 
+arr2 = [] 
+arr3 = [] 
+cards0 = [] 
+cards1 = [] 
+cards2 = [] 
+cards3 = [] 
+ 
+function flag(country){
+    return `<i class="flag flag-${country}"></i>`
+}
+ 
+function geraGolsForte(array){ 
+    while (array.length < 3){ 
+        var gols=[0,1,2,3,4,5,6,7,8]
+        var golsponderados = [0.2, 0.4, 2, 3.5, 3.5, 0.1, 0.1, 0.1, 0.1]
+        var totalponderado = eval(golsponderados.join("+"))
+        var ponderacao = new Array()
+        var golatual = 0;
+    
+        while (golatual<gols.length){ //step through each fruit[] element
+            for (i=0; i<golsponderados[golatual]; i++)
+                ponderacao[ponderacao.length]=gols[golatual]
+            golatual++
+        }
+        var randomnumber=Math.floor(Math.random()*totalponderado)
+
+         array.push(ponderacao[randomnumber]) 
+    } 
+} 
+
+function geraGolsMed(array){ 
+    while (array.length < 3){ 
+        var gols=[0,1,2,3,4]
+        var golsponderados = [4.0,4.0,1.0,0.5,0.5]
+        var totalponderado = eval(golsponderados.join("+"))
+        var ponderacao = new Array();
+        var golatual = 0;
+    
+        while (golatual<gols.length){ //step through each fruit[] element
+            for (i=0; i<golsponderados[golatual]; i++)
+                ponderacao[ponderacao.length]=gols[golatual]
+            golatual++
+        }
+        var randomnumber=Math.floor(Math.random()*totalponderado)
+
+         array.push(ponderacao[randomnumber]) 
+    }
+}
+
+function geraGolsFraco(array){ 
+    while (array.length < 3){ 
+        var gols=[0,1,2,3]
+        var golsponderados = [5.2,4.3,0.3, 0.2]
+        var totalponderado = eval(golsponderados.join("+"))
+        var ponderacao = new Array();
+        var golatual = 0;
+    
+        while (golatual<gols.length){ //step through each fruit[] element
+            for (i=0; i<golsponderados[golatual]; i++)
+                ponderacao[ponderacao.length]=gols[golatual]
+            golatual++
+        }
+        var randomnumber=Math.floor(Math.random()*totalponderado)
+
+         array.push(ponderacao[randomnumber]) 
+    } 
+} 
+ 
+function geraCartoes(array){ 
+    while (array.length < 3){ 
+        array.push(Math.floor(Math.random() * (3 - 0) ) + 0) 
+    } 
+} 
+ 
+geraGolsForte(arr0) 
+geraGolsFraco(arr1) 
+geraGolsMed(arr2) 
+geraGolsMed(arr3) 
+geraCartoes(cards0) 
+geraCartoes(cards1) 
+geraCartoes(cards2) 
+geraCartoes(cards3) 
+ 
+//console.log(arr) 
+//console.log(cards) 
+ 
+let grupoA = [ 
+    {flag: "",  nome: 'Portugal', jogos:3, pontos: [], pontuacao_final:0, gols: arr0, gp: [], gc: [], saldo: 0, cartoes:cards0, soma_gols: 0, soma_cartoes: 0, soma_gc: 0, v: 0, e: 0, d: 0}, 
+    {flag: "", nome: 'Gana', jogos:3, pontos: [], pontuacao_final:0, gols: arr1, gp: [], gc: [], saldo: 0, cartoes:cards1, soma_gols: 0, soma_cartoes: 0,soma_gc: 0, v: 0, e: 0, d: 0}, 
+    {flag: "", nome: 'Uruguai', jogos:3, pontos: [], pontuacao_final:0, gols: arr2, gp: [], gc: [], saldo: 0, cartoes:cards2, soma_gols: 0, soma_cartoes: 0,soma_gc: 0, v: 0, e: 0, d: 0}, 
+    {flag: "",  nome: 'Coréia do Sul', jogos:3, pontos: [], pontuacao_final:0, gols: arr3, gp: [], gc: [], saldo: 0, cartoes:cards3, soma_gols: 0, soma_cartoes: 0,soma_gc: 0, v: 0, e: 0, d: 0}, 
+] 
+ 
+ 
+console.log(grupoA[0].gols); 
+console.log(grupoA[1].gols); 
+console.log(grupoA[2].gols); 
+console.log(grupoA[3].gols); 
+/* 
+console.log('//////////////') 
+console.log(grupoA[0].cartoes); 
+console.log(grupoA[1].cartoes); 
+console.log(grupoA[2].cartoes); 
+console.log(grupoA[3].cartoes); 
+console.log('//////////////')*/ 
+ 
+//SOMA GOLS 
+let soma = grupoA.map(g => g.gols.reduce((acc, next) => acc+=next, 0)); 
+grupoA[0]['soma_gols'] = soma[0] 
+grupoA[1]['soma_gols'] = soma[1] 
+grupoA[2]['soma_gols'] = soma[2] 
+grupoA[3]['soma_gols'] = soma[3] 
+ 
+//SOMA CARTOES 
+let cartoes = grupoA.map(g => g.cartoes.reduce((acc, next) => acc+=next, 0)); 
+grupoA[0]['soma_cartoes'] = cartoes[0] 
+grupoA[1]['soma_cartoes'] = cartoes[1] 
+grupoA[2]['soma_cartoes'] = cartoes[2] 
+grupoA[3]['soma_cartoes'] = cartoes[3] 
+ 
+ 
+//SALDO DE GOLS 
+grupoA[0]['gp'] = soma[0] 
+grupoA[1]['gp'] = soma[1] 
+grupoA[2]['gp'] = soma[2] 
+grupoA[3]['gp'] = soma[3] 
+//console.log(grupoA) 
+ 
+//GOLS CONTRA  
+let gol1_0 = grupoA[1]['gols'][0] 
+let gol2_0 = grupoA[2]['gols'][1] 
+let gol3_0 = grupoA[3]['gols'][2] 
+ 
+let somagc_0 = []; 
+somagc_0.push(gol1_0) 
+somagc_0.push(gol2_0) 
+somagc_0.push(gol3_0) 
+////////////////////////////////// 
+let gol1_1 = grupoA[0]['gols'][0] 
+let gol2_1 = grupoA[2]['gols'][2] 
+let gol3_1 = grupoA[3]['gols'][1] 
+ 
+let somagc_1 = []; 
+somagc_1.push(gol1_1) 
+somagc_1.push(gol2_1) 
+somagc_1.push(gol3_1) 
+////////////////////////////////// 
+let gol1_2 = grupoA[0]['gols'][1] 
+let gol2_2 = grupoA[1]['gols'][2] 
+let gol3_2 = grupoA[3]['gols'][0] 
+ 
+let somagc_2 = []; 
+somagc_2.push(gol1_2) 
+somagc_2.push(gol2_2) 
+somagc_2.push(gol3_2) 
+//////////////////////////////// 
+ 
+let gol1_3 = grupoA[0]['gols'][2] 
+let gol2_3 = grupoA[1]['gols'][1] 
+let gol3_3 = grupoA[2]['gols'][0] 
+ 
+let somagc_3 = []; 
+somagc_3.push(gol1_3) 
+somagc_3.push(gol2_3) 
+somagc_3.push(gol3_3) 
+ 
+grupoA[0]['gc'] = somagc_0 
+grupoA[1]['gc'] = somagc_1 
+grupoA[2]['gc'] = somagc_2 
+grupoA[3]['gc'] = somagc_3 
+ 
+let somaGolsContra = grupoA.map(g => g.gc.reduce((acc, next) => acc += next ), 0) 
+ 
+grupoA[0]['soma_gc'] = somaGolsContra[0] 
+grupoA[1]['soma_gc'] = somaGolsContra[1] 
+grupoA[2]['soma_gc'] = somaGolsContra[2] 
+grupoA[3]['soma_gc'] = somaGolsContra[3] 
+ 
+//console.log(grupoA); 
+ 
+//SALDO DE GOLS  
+let saldo = grupoA.map(g => g.soma_gols - g.soma_gc); 
+grupoA[0]['saldo'] = saldo[0] 
+grupoA[1]['saldo'] = saldo[1] 
+grupoA[2]['saldo'] = saldo[2] 
+grupoA[3]['saldo'] = saldo[3] 
+ 
+
+//SOMA_PONTOS
+if(grupoA[0]['gols'][0] > grupoA[1]['gols'][0]){
+    grupoA[0]['pontos'].push(3)
+    grupoA[0]['v']++
+}
+if(grupoA[0]['gols'][1] > grupoA[2]['gols'][1]){
+    grupoA[0]['pontos'].push(3)
+    grupoA[0]['v']++
+}
+if(grupoA[0]['gols'][2] > grupoA[3]['gols'][2]){
+    grupoA[0]['pontos'].push(3)
+    grupoA[0]['v']++
+}
+
+if(grupoA[0]['gols'][0] == grupoA[1]['gols'][0]){
+    grupoA[0]['pontos'].push(1)
+    grupoA[0]['e']++
+}
+if(grupoA[0]['gols'][1] == grupoA[2]['gols'][1]){
+    grupoA[0]['pontos'].push(1)
+    grupoA[0]['e']++
+}
+if(grupoA[0]['gols'][2] == grupoA[3]['gols'][2]){
+    grupoA[0]['pontos'].push(1)
+    grupoA[0]['e']++
+}
+
+if(grupoA[0]['gols'][0] < grupoA[1]['gols'][0]){
+    grupoA[0]['pontos'].push(0)
+    grupoA[0]['d']++
+}
+if(grupoA[0]['gols'][1] < grupoA[2]['gols'][1]){
+    grupoA[0]['pontos'].push(0)
+    grupoA[0]['d']++
+}
+if(grupoA[0]['gols'][2] < grupoA[3]['gols'][2]){
+    grupoA[0]['pontos'].push(0)
+    grupoA[0]['d']++
+}
+/////////////////////////////////////////////////////////////////
+
+if(grupoA[1]['gols'][0] > grupoA[0]['gols'][0]){
+    grupoA[1]['pontos'].push(3)
+    grupoA[1]['v']++
+}
+if(grupoA[1]['gols'][1] > grupoA[2]['gols'][2]){
+    grupoA[1]['pontos'].push(3)
+    grupoA[1]['v']++
+}
+if(grupoA[1]['gols'][2] > grupoA[3]['gols'][1]){
+    grupoA[1]['pontos'].push(3)
+    grupoA[1]['v']++
+}
+
+if(grupoA[1]['gols'][0] == grupoA[0]['gols'][0]){
+    grupoA[1]['pontos'].push(1)
+    grupoA[1]['e']++
+}
+if(grupoA[1]['gols'][1] == grupoA[2]['gols'][2]){
+    grupoA[1]['pontos'].push(1)
+    grupoA[1]['e']++
+}
+if(grupoA[1]['gols'][2] == grupoA[3]['gols'][1]){
+    grupoA[1]['pontos'].push(1)
+    grupoA[1]['e']++
+}
+
+if(grupoA[1]['gols'][0] < grupoA[0]['gols'][0]){
+    grupoA[1]['pontos'].push(0)
+    grupoA[1]['d']++
+}
+if(grupoA[1]['gols'][1] < grupoA[2]['gols'][2]){
+    grupoA[1]['pontos'].push(0)
+    grupoA[1]['d']++
+}
+if(grupoA[1]['gols'][2] < grupoA[3]['gols'][1]){
+    grupoA[1]['pontos'].push(0)
+    grupoA[1]['d']++
+}
+
+/////////////////////////////////////////////////////////////////
+
+if(grupoA[2]['gols'][0] > grupoA[0]['gols'][1]){
+    grupoA[2]['pontos'].push(3)
+    grupoA[2]['v']++
+}
+if(grupoA[2]['gols'][1] > grupoA[1]['gols'][2]){
+    grupoA[2]['pontos'].push(3)
+    grupoA[2]['v']++
+}
+if(grupoA[2]['gols'][2] > grupoA[3]['gols'][0]){
+    grupoA[2]['pontos'].push(3)
+    grupoA[2]['v']++
+}
+
+if(grupoA[2]['gols'][0] == grupoA[0]['gols'][1]){
+    grupoA[2]['pontos'].push(1)
+    grupoA[2]['e']++
+}
+if(grupoA[2]['gols'][1] == grupoA[1]['gols'][2]){
+    grupoA[2]['pontos'].push(1)
+    grupoA[2]['e']++
+}
+if(grupoA[2]['gols'][2] == grupoA[3]['gols'][0]){
+    grupoA[2]['pontos'].push(1)
+    grupoA[2]['e']++
+}
+
+if(grupoA[2]['gols'][0] < grupoA[0]['gols'][1]){
+    grupoA[2]['pontos'].push(0)
+    grupoA[2]['d']++
+}
+if(grupoA[2]['gols'][1] < grupoA[1]['gols'][2]){
+    grupoA[2]['pontos'].push(0)
+    grupoA[2]['d']++
+}
+if(grupoA[2]['gols'][2] < grupoA[3]['gols'][0]){
+    grupoA[2]['pontos'].push(0)
+    grupoA[2]['d']++
+}
+
+/////////////////////////////////////////////////////////////////
+
+if(grupoA[3]['gols'][0] > grupoA[0]['gols'][2]){
+    grupoA[3]['pontos'].push(3)
+    grupoA[3]['v']++
+}
+if(grupoA[3]['gols'][1] > grupoA[1]['gols'][1]){
+    grupoA[3]['pontos'].push(3)
+    grupoA[3]['v']++
+}
+if(grupoA[3]['gols'][2] > grupoA[2]['gols'][0]){
+    grupoA[3]['pontos'].push(3)
+    grupoA[3]['v']++
+}
+
+if(grupoA[3]['gols'][0] == grupoA[0]['gols'][2]){
+    grupoA[3]['pontos'].push(1)
+    grupoA[3]['e']++
+}
+if(grupoA[3]['gols'][1] == grupoA[1]['gols'][1]){
+    grupoA[3]['pontos'].push(1)
+    grupoA[3]['e']++
+}
+if(grupoA[3]['gols'][2] == grupoA[2]['gols'][0]){
+    grupoA[3]['pontos'].push(1)
+    grupoA[3]['e']++
+}
+
+if(grupoA[3]['gols'][0] < grupoA[0]['gols'][2]){
+    grupoA[3]['pontos'].push(0)
+    grupoA[3]['d']++
+}
+if(grupoA[3]['gols'][1] < grupoA[1]['gols'][1]){
+    grupoA[3]['pontos'].push(0)
+    grupoA[3]['d']++
+}
+if(grupoA[3]['gols'][2] < grupoA[2]['gols'][0]){
+    grupoA[3]['pontos'].push(0)
+    grupoA[3]['d']++
+}
+
+let soma_pontos = grupoA.map(g => g.pontos.reduce((acc, next) => acc += next), 0)
+grupoA[0]['pontuacao_final'] = soma_pontos[0];
+grupoA[1]['pontuacao_final'] = soma_pontos[1];
+grupoA[2]['pontuacao_final'] = soma_pontos[2];
+grupoA[3]['pontuacao_final'] = soma_pontos[3];
+
+
+let classificacao = _.orderBy(grupoA, ['pontuacao_final', 'v', 'saldo', 'gp', 'e', 'soma_cartoes'], ['desc', 'desc', 'desc', 'desc', 'desc', 'asc'])
+
+// console.log(classificacao, "TIMES CLASSIFICADOS")
+
+// let match = grupoA[0].nome + " " + grupoA[0].gols[0] + " x " + grupoA[1].gols[0]  + " " + grupoA[1].nome
+
+// console.log(match)
+
+const h1 = classificacao[0]
+const h2 = classificacao[1]
+
+module.exports = {h1, h2}
